a77660d6ad399419e80fd2a6b58d03f905bd8c24
Max Camacho <161536974+maxkmacho@users.noreply.github.com>
2026-03-27 11:46:14 -0500

    Create server.js

---
+++ backend/maxc/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login_page.html"));
+});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
